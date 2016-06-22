ab108443e981c51cfd0fdef446700f9dc3aaa5e6
rolandovillca <rolandovillca@gmail.com>
2016-06-22 09:32:52 -0400

    Adding examples for VPC and filters

--- services/vpc/sample.py
+++ services/vpc/sample.py
@@ -1,9 +1,9 @@
 '''
 ====================
-Boto 3 - EC2 Example
+Boto 3 - IAM Example
 ====================
-This application implements the EC2 service that lets you gets
-information from Amazon EC2. See the README for more details.
+This application implements the VPC service that lets you gets
+information from Amazon VPC. See the README for more details.
 '''
 import boto3
 import json
@@ -15,12 +15,13 @@ AWS_ACCESS_KEY_ID = credentials['aws_access_key_id']
 AWS_SECRET_ACCESS_KEY = credentials['aws_secret_access_key']
 REGION_NAME = 'us-west-1'
 
-ec2 = boto3.client('ec2',
+iam = boto3.client('iam',
                     aws_access_key_id=AWS_ACCESS_KEY_ID,
                     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
                     region_name=REGION_NAME)
 
-print ec2.describe_regions()
-print
-print ec2.describe_instances()
-print
+# Filtering VPCs by tags:
+filters = [{'Name':'tag:Name', 'Values':['webapp01']}]
+
+webapp01 = list(ec2.vpcs.filter(Filters=filters))[0]
+webapp01.vpc_id
