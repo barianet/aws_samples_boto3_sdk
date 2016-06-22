92cb404b96e252c15547593e273e0ab15aa74e14
rolandovillca <rolandovillca@gmail.com>
2016-06-22 14:12:43 -0400

    Adding sample for VPC service

--- services/vpc/sample.py
+++ services/vpc/sample.py
@@ -1,6 +1,6 @@
 '''
 ====================
-Boto 3 - IAM Example
+Boto 3 - VPC Example
 ====================
 This application implements the VPC service that lets you gets
 information from Amazon VPC. See the README for more details.
@@ -15,13 +15,8 @@ AWS_ACCESS_KEY_ID = credentials['aws_access_key_id']
 AWS_SECRET_ACCESS_KEY = credentials['aws_secret_access_key']
 REGION_NAME = 'us-west-1'
 
-iam = boto3.client('iam',
+vpc = boto3.client('vpc',
                     aws_access_key_id=AWS_ACCESS_KEY_ID,
                     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
                     region_name=REGION_NAME)
 
-# Filtering VPCs by tags:
-filters = [{'Name':'tag:Name', 'Values':['webapp01']}]
-
-webapp01 = list(ec2.vpcs.filter(Filters=filters))[0]
-webapp01.vpc_id
